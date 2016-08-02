8b85d9bd03e1224749b6fb929726f20ef684f1e1
steveinatorx <steveinatorx@gmail.com>
2016-08-01 22:26:43 -0700

    dynamically render local data props

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,24 +1,35 @@
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+
 var CommentBox = React.createClass({
   render: function() {
     return (
      <div className="commentBox">
      <h1>Comments</h1>
-     <CommentList />
+     <CommentList data={this.props.data} />
      <CommentForm />
      </div>
     );
   }
 });
-
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
-     <div className="commentList">
-       <Comment author="Pete Hunt">This is one comment</Comment>
-       <Comment author="Jordan Walke">This is *another* comment</Comment>
+      <div className="commentList">
+        {commentNodes}
       </div>
     );
-  } 
+  }
 });
 
 var CommentForm = React.createClass({
@@ -32,19 +43,24 @@ var CommentForm = React.createClass({
 });
 
 var Comment = React.createClass({
-  render: function() {
+ rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+render: function() {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
-});
+}); 
 
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={data}/>,
     document.getElementById('content')
 );
